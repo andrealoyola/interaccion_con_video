f95b8a8de954790c56fe2bb6e86f2fa00cc0f3e1
ANDREA CRISTINA LOYOLA DELGADO <andrea.loyolad@ucuenca.edu.ec>
2025-01-23 10:10:20 -0500

    se actualizaron las imagenes y sketch

--- sketch.js
+++ sketch.js
@@ -4,23 +4,18 @@ let hands = [];
 let branchImagesYes = []; // Imágenes para las ramas de "Sí"
 let branchImagesNo = []; // Imágenes para las ramas de "No"
 let trunkImage; // Imagen del tronco
+let treeTopImage; // Imagen de la copa del árbol
 let tree = [];
 let messages = [
-  "¿Te sientes optimista hoy?",
-  "¿Prefieres trabajar solo?",
-  "¿Tomarías un riesgo hoy?",
-  "¿Crees que las cosas mejorarán con esfuerzo?",
-  "¿Te gusta explorar nuevas ideas?",
-  "¿Sueles adaptarte rápido a los cambios?",
-  "¿Confías en tu intuición?",
-  "¿Te atreverías a aprender algo completamente nuevo?",
-  "¿Eres de tomar decisiones rápidas?",
-  "¿Te sientes cómodo trabajando en equipo?",
-  "¿Consideras importante la perseverancia?",
-  "¿Crees en el poder de la reflexión?",
-  "¿Estás dispuesto a cambiar tu perspectiva?",
-  "¿Prefieres soluciones creativas?",
-  "¿Te gustaría que el árbol crezca de forma única?",
+  "¿Te sientes en paz contigo mismo en este momento?",
+  "¿Te permites expresar tus emociones libremente?",
+  "¿Te sientes valorado por las personas cercanas a ti?",
+  "¿Sientes que recibes el apoyo emocional que necesitas?",
+  "¿Haces algo que disfrutes verdaderamente todos los días?",
+  "¿Te entusiasma pensar en tu futuro?",
+  "¿Sientes que estás aprovechando al máximo tus talentos?",
+  "¿Te sientes agradecido por lo que tienes en la vida?",
+  "¿Te has perdonado por errores pasados?",
 ];
 let message =
   "Cada decisión que tomes hará crecer un árbol único. ¿Estás listo para comenzar?";
@@ -32,14 +27,15 @@ let lastActionTime = 0;
 function preload() {
   handPose = ml5.handPose();
   trunkImage = loadImage("tronco.png"); // Carga la imagen del tronco
+  treeTopImage = loadImage("top_branch.png"); // Carga la imagen de la copa del árbol
 
   // Carga las imágenes de las ramas para "Sí"
-  for (let i = 1; i <= 11; i++) {
+  for (let i = 1; i <= 7; i++) {
     branchImagesYes.push(loadImage(`branch_yes_${i}.png`));
   }
 
   // Carga las imágenes de las ramas para "No"
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 8; i++) {
     branchImagesNo.push(loadImage(`branch_no_${i}.png`));
   }
 }
@@ -56,7 +52,7 @@ function gotHands(results) {
 }
 
 function setup() {
-  let canvas = createCanvas(1200, 1000);
+  let canvas = createCanvas(1420, 1000);
   canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
   video = createCapture(VIDEO);
   video.size(640, 480);
@@ -78,12 +74,24 @@ function draw() {
   // Dibuja la imagen del tronco en la base del lienzo
   let trunkWidth = trunkImage.width / 2; // Tamaño del tronco ajustado
   let trunkHeight = trunkImage.height;
-  image(trunkImage, width - 900 / 2 - trunkWidth / 2, height - trunkHeight);
+  image(trunkImage, width - 850 / 2 - trunkWidth / 2, height - trunkHeight);
 
   // Dibuja las ramas
   for (let i = 0; i < tree.length; i++) {
     let branch = tree[i];
     image(branch.image, branch.x, branch.y);
+  }
+
+  // Dibuja la copa del árbol si el árbol está completo
+  if (decisionStep >= messages.length) {
+    let topWidth = treeTopImage.width - 750 / 2;
+    let topHeight = treeTopImage.height;
+
+    // Calcula la posición para que el borde superior de la copa toque el borde superior del canvas
+    let topX = width / 2 - topWidth / 2;
+    let topY = 0; // Borde superior del canvas
+
+    image(treeTopImage, topX, topY);
   }
 }
 
@@ -109,9 +117,9 @@ function isFist(keypoints) {
 
 function handleYes() {
   if (decisionStep < messages.length) {
-    let yOffset = height - decisionStep * 84;
+    let yOffset = height - 200 - decisionStep * 80;
     tree.push({
-      x: width / 2 + 200, // Fija la posición de X a la derecha
+      x: width / 2 + 222, // Fija la posición de X a la derecha
       y: yOffset,
       image: random(branchImagesYes),
     });
@@ -121,9 +129,9 @@ function handleYes() {
 
 function handleNo() {
   if (decisionStep < messages.length) {
-    let yOffset = height - decisionStep * 84;
+    let yOffset = height - 200 - decisionStep * 80;
     tree.push({
-      x: width / 2 + 50, // Fija la posición de X a la izquierda
+      x: width / 2 - 32, // Fija la posición de X a la izquierda
       y: yOffset, // La Y cambia con cada decisión
       image: random(branchImagesNo),
     });
